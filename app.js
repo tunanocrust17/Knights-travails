6d8199cf54196b5175271130363ab32e57ae79e7
ewhuet <ewhuet@gmail.com>
2024-05-13 21:10:25 +0300

    finalized the knightMoves function to return the number of moves it will take a knight to move between positions on the board using breadth first search and graphs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function Node(pos, path){
     if(
         pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7
     ) {
-        return
+        return null
     }
     return {pos, path}
 }
@@ -13,6 +13,36 @@ function knightMoves([x,y], [a,b]){
 
     let queue = [Node([x,y], [[x,y]])]
     let currentNode = queue.shift();
+    
+   
+    while(currentNode.pos[0] !== a || currentNode.pos[1] !== b) {
+        let possibleMoves = [
+            [currentNode.pos[0] + 2, currentNode.pos [1] + 1],
+            [currentNode.pos[0] + 2, currentNode.pos [1] - 1],
+            [currentNode.pos[0] - 2, currentNode.pos [1] + 1],
+            [currentNode.pos[0] - 2, currentNode.pos [1] - 1],
+            [currentNode.pos[0] + 1, currentNode.pos [1] + 2],
+            [currentNode.pos[0] + 1, currentNode.pos [1] - 2],
+            [currentNode.pos[0] - 1, currentNode.pos [1] + 2],
+            [currentNode.pos[0] - 1, currentNode.pos [1] - 2]  
+        ]
+    
+        possibleMoves.forEach((jump)=>{
+            let node = Node(jump, currentNode.path.concat([jump]))
+            if(node){
+                queue.push(node)
+            }
+        })
+        currentNode = queue.shift();
+    }
+  
+    console.log("You did it in " + (currentNode.path.length-1) + " moves!")
+    for(let i = 0 ; i < currentNode.path.length ; i++) {
+        console.log(currentNode.path[i])
+    }
     return currentNode
 }
-console.log(knightMoves([3,4], [7,7]))
+knightMoves([1,3], [5,5])
+knightMoves([3,3], [4,3])
+knightMoves([1,7], [3,4])
+
